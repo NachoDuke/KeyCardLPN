f5a9b7def7658c465e6e638c6edb83797322d8af
NachoDuke <premankur.qb9@gmail.com>
2022-10-06 18:53:36 +0530

    Made sure keys are secure

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from src.lock import *
 if __name__=="__main__":
     card=Card()
     lock=Lock()
+    numKeys=50
     p=0.3
     length=1000
-    key=getBinaryVector(length)
-    card.addData(key,p)
-    lock.addData(key,p)
-    print(lock.unlock(card))
+    keys=[]
+    for i in range(numKeys):
+        keys.append(getBinaryVector(length))
+    card.addData(keys[0],p)
+    count=0
+    for i in range(numKeys):
+        lock.addData(keys[i],p)
+        if(lock.unlock(card)):
+            count+=1
+    print(count)
